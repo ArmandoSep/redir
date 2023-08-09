23cb93447d10d03bb17794534b7257f4bdf3baaf
HelloSWF <hello@sellwithoutfixing.com>
2023-08-09 01:24:58 -0600

    updated index.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,20 +2,26 @@ import { NowRequest, NowResponse } from '@vercel/node';
 import axios from 'axios';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  try {
-    // Extract the page name from the URL path
-    const pageName = req.url.replace(/^\//, '');
+    try {
+        // Extract the page name from the URL path
+        const pageName = req.url.replace(/^\//, '');
+        // If there's a page name, perform the API request and redirect
+        if (pageName) {
+            // Make a POST API request (replace with your actual API endpoint)
+            const apiResponse = await axios.post(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir`, {
+            pageName: pageName,
+            });
 
-    // Make a POST API request (replace with your actual API endpoint)
-    const apiResponse = await axios.post(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir`, {
-      pageName: pageName,
-    });
-
-    // Redirect to the response page with the API data
-    res.setHeader('Location', `/response/${pageName}?data=${encodeURIComponent(apiResponse.data)}`);
-    res.status(302).send('');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('An error occurred');
-  }
+            // Redirect to the response page with the API data
+            res.setHeader('Location', `/response/${pageName}?data=${encodeURIComponent(apiResponse.data)}`);
+            res.status(302).send('');
+        } else {
+            // If no page name, redirect to www.getownerfinanced.com
+            res.setHeader('Location', 'https://www.getownerfinanced.com');
+            res.status(302).send('');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
 };
