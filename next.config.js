e581ae8b67b68e8cc1032a413a028cd8ee182d04
HelloSWF <hello@sellwithoutfixing.com>
2023-08-10 04:11:34 -0600

    modified next config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,16 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  async rewrites() {
+    return [
+      // Handle redirection and API call for pages with slugs
+      {
+        source: '/:slug', // Match any path with a slug
+        destination: '/api/[slug]', // Redirect to the custom API route
+      },
+    ];
+  },
+};
