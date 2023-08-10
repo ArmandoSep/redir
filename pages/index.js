484adeff2e802b8b0f5cb314dd2e833b03deb2f1
HelloSWF <hello@sellwithoutfixing.com>
2023-08-10 04:38:42 -0600

    fixed index.js

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,12 @@ export default function Home() {
     // Empty return or loading indicator
     return null;
   }
-  
+
+export async function getServerSideProps({ req }) {
+    return {
+      redirect: {
+        destination: 'https://www.getownerfinanced.com',
+        permanent: false,
+      },
+    };
+  }  
