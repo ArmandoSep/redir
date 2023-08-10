96e9e2eb29be42ab53a49fb95c9670c92dc666e7
HelloSWF <hello@sellwithoutfixing.com>
2023-08-10 04:31:49 -0600

    fixed slug.js

--- pages/api/[slug].js
+++ pages/api/[slug].js
@@ -1,54 +1,36 @@
-// export default async function handler(req, res) {
-//     const { slug } = req.query;
-  
-//     if (slug) {
-//       try {
-//         const response = await fetch(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir?r=${slug}`);
-//         const responseData = await response.json();
-  
-//         const redirectUrl = responseData.redirectUrl || 'https://www.getownerfinanced.com';
-  
-//         res.writeHead(302, { Location: redirectUrl });
-//         res.end();
-//       } catch (error) {
-//         console.error('Error:', error);
-//         res.writeHead(302, { Location: 'https://www.getownerfinanced.com' });
-//         res.end();
-//       }
-//     } else {
-//       res.writeHead(302, { Location: 'https://www.getownerfinanced.com' });
-//       res.end();
-//     }
-//   }
-  
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
     const { slug } = req.query;
 
     if (slug) {
-        try {
-          const response = await fetch(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ slug }),
-          });
-  
-          const responseData = await response.json();
-  
-          const redirectUrl = responseData.redirectUrl || 'https://www.getownerfinanced.com';
-  
-          res.writeHead(302, { Location: redirectUrl });
-          res.end();
-        } catch (error) {
-          console.error('Error:', error);
-          res.writeHead(302, { Location: 'https://www.getownerfinanced.com' });
-          res.end();
-        }
-    } else {
+      try {
+        // Get user's IP address from the request headers
+        const IP = req.headers['x-real-ip'] || req.connection.remoteAddress;
+
+        // Make a request to your endpoint to get the redirect link based on the slug
+        const response = await fetch(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir?r=${slug}&i=${IP}`);
+        const responseData = await response.json();
+
+        // Get the redirect URL from the response data
+        const redirectUrl = responseData.redirectUrl || 'https://www.getownerfinanced.com';
+
+        // Redirect immediately on the server side
+        res.writeHead(302, { Location: redirectUrl });
+        res.end();
+      } catch (error) {
+        console.error('Error:', error);
         res.writeHead(302, { Location: 'https://www.getownerfinanced.com' });
         res.end();
+      }
+    } else {
+      res.writeHead(302, { Location: 'https://www.getownerfinanced.com' });
+      res.end();
     }
-}
+} 
+
+
+
+
+
   
--- pages/index.js
+++ pages/index.js
@@ -3,38 +3,3 @@ export default function Home() {
     return null;
   }
   
-  export async function getServerSideProps({ req }) {
-    // Get the current path from the request object
-    const currentPath = req.url;
-  
-    // Extract the page slug from the path
-    const pageSlug = currentPath.substring(1); // Remove the leading slash
-  
-    // If a page slug exists, make a GET request
-    if (pageSlug) {
-      try {
-        const response = await fetch(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir?r=${pageSlug}`);
-        const responseData = await response.json();
-  
-        const redirectUrl = responseData.redirectUrl || 'https://www.getownerfinanced.com';
-  
-        return {
-          redirect: {
-            destination: redirectUrl,
-            permanent: false,
-          },
-        };
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    }
-  
-    // If no page slug or an error occurred, redirect to the default URL
-    return {
-      redirect: {
-        destination: 'https://www.getownerfinanced.com',
-        permanent: false,
-      },
-    };
-  }
-  
