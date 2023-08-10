e2e8f9f7a09e8d7e329420c04046937e1e0a8cb5
HelloSWF <hello@sellwithoutfixing.com>
2023-08-10 03:51:16 -0600

    updated index.js -- faster redirect

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     // If a page slug exists, make a POST request
     if (pageSlug) {
       const data = { pageSlug };
-
+        console.log(pageSlug);
       fetch('https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir', {
         method: 'POST',
         headers: {
@@ -38,5 +38,45 @@ export default function Home() {
     }
   }, []);
 
-  return <div>Redirecting...</div>;
+  // Empty return or loading indicator
+  return null;
 }
+
+// Outside the component, perform the redirection immediately
+if (typeof window !== 'undefined') {
+  // Get the current path from the URL
+  const currentPath = window.location.pathname;
+
+  // Extract the page slug from the path
+  const pageSlug = currentPath.substring(1); // Remove the leading slash
+
+  // If a page slug exists, redirect immediately
+  if (pageSlug) {
+    const data = { pageSlug };
+
+    fetch('https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        const redirectUrl = responseData.redirectUrl;
+        if (redirectUrl) {
+          window.location.href = redirectUrl;
+        } else {
+          window.location.href = 'https://www.getownerfinanced.com';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        window.location.href = 'https://www.getownerfinanced.com';
+      });
+  } else {
+    // If no page slug, redirect to the default URL
+    window.location.href = 'https://www.getownerfinanced.com';
+  }
+}
+
