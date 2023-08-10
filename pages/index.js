fce37529b6d32588a07c30f5d21bb110098adb35
HelloSWF <hello@sellwithoutfixing.com>
2023-08-10 03:57:05 -0600

    updated index.js -- changed to get requestt

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,40 @@
-import { useEffect } from 'react';
-
 export default function Home() {
-  useEffect(() => {
-    // Get the current path from the URL
-    const currentPath = window.location.pathname;
-
+    // Empty return or loading indicator
+    return null;
+  }
+  
+  export async function getServerSideProps({ req }) {
+    // Get the current path from the request object
+    const currentPath = req.url;
+  
     // Extract the page slug from the path
     const pageSlug = currentPath.substring(1); // Remove the leading slash
-
-    // If a page slug exists, make a POST request
+  
+    // If a page slug exists, make a GET request
     if (pageSlug) {
-      const data = { pageSlug };
-        console.log(pageSlug);
-      fetch('https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-        .then(response => response.json())
-        .then(responseData => {
-          const redirectUrl = responseData.redirectUrl;
-          if (redirectUrl) {
-            window.location.href = redirectUrl;
-          } else {
-            window.location.href = 'https://www.getownerfinanced.com';
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          window.location.href = 'https://www.getownerfinanced.com';
-        });
-    } else {
-      // If no page slug, redirect to the default URL
-      window.location.href = 'https://www.getownerfinanced.com';
-    }
-  }, []);
-
-  // Empty return or loading indicator
-  return null;
-}
-
-// Outside the component, perform the redirection immediately
-if (typeof window !== 'undefined') {
-  // Get the current path from the URL
-  const currentPath = window.location.pathname;
-
-  // Extract the page slug from the path
-  const pageSlug = currentPath.substring(1); // Remove the leading slash
-
-  // If a page slug exists, redirect immediately
-  if (pageSlug) {
-    const data = { pageSlug };
-
-    fetch('https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then(response => response.json())
-      .then(responseData => {
-        const redirectUrl = responseData.redirectUrl;
-        if (redirectUrl) {
-          window.location.href = redirectUrl;
-        } else {
-          window.location.href = 'https://www.getownerfinanced.com';
-        }
-      })
-      .catch(error => {
+      try {
+        const response = await fetch(`https://gnetia18l1.execute-api.us-east-1.amazonaws.com/default/GOF_redir?r=${pageSlug}`);
+        const responseData = await response.json();
+  
+        const redirectUrl = responseData.redirectUrl || 'https://www.getownerfinanced.com';
+  
+        return {
+          redirect: {
+            destination: redirectUrl,
+            permanent: false,
+          },
+        };
+      } catch (error) {
         console.error('Error:', error);
-        window.location.href = 'https://www.getownerfinanced.com';
-      });
-  } else {
-    // If no page slug, redirect to the default URL
-    window.location.href = 'https://www.getownerfinanced.com';
+      }
+    }
+  
+    // If no page slug or an error occurred, redirect to the default URL
+    return {
+      redirect: {
+        destination: 'https://www.getownerfinanced.com',
+        permanent: false,
+      },
+    };
   }
-}
-
+  
